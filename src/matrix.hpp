d11d21d881794c61bc2d761b215e2dae6ed68777
Andrew Bass <andybass009@gmail.com>
2015-12-27 00:00:44 -0500

    Add documentation

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+/**
+ * Interface to a LED matrix
+ * Doesn't support shift register based matrices currently
+ * Assumes LEDs are connected in sequential ports
+ */
 struct Matrix {
     bool* buffer;
 
@@ -11,10 +16,17 @@ struct Matrix {
  
     Matrix(int startX, int startY, int width, int height);
 
+    // Allows for indexing as such: myMatrix[row][col]
     bool* operator[] (int row);
 
+    // Turns all values in "buffer" off
     void clear();
 
+    // Used By "write" to physicially turn off LEDs
     void preparePins();
+
+    // Updates physical LED matrix
+    // Doesn't keep the matrix powered on after being called
+    // Call in "loop" or equivalent function to keep matrix powered on
     void write(); 
 };
