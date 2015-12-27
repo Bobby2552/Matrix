7be3ae793a451423c6ffbaad2b550e71345d78de
Andrew Bass <andybass009@gmail.com>
2015-12-26 22:26:07 -0500

    Add graphing calculator

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,46 @@
 #include "Arduino.h"
 #include "matrix.hpp"
 
+#define COUNT(arr) (sizeof(arr) / sizeof(*arr))
+
 Matrix mat(30, 2, 10, 5);
 
+double benSin(double x) {
+    return (2.5 * sin(x)) + 2;
+}
+
+double benSqrt(double x) {
+    return sqrt(x);
+}
+
+double benSquared(double x) {
+    x -= 5;
+    return x * x;
+}
+
+typedef double (*function)(double);
+
+function fns[] = { benSin, benSqrt, benSquared };
+
+int i = COUNT(fns);
+int timeToDisplay = 5000;
+
 void setup() {
-    mat[0][0] = HIGH;
-    mat[0][1] = HIGH;
+
 }
 
 void loop() {
+    if (millis() - timeToDisplay * i < 100) {
+        i++;
+    }
+
+    function f = fns[i % COUNT(fns)];
+
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 5; y++) {
+            mat[y][x] = y == (int)f(x) ? HIGH:LOW;
+        }
+    }
+
     mat.write();
 }
